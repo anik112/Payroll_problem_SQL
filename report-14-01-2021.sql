587d89e4bdb615b76f5c0915aea0fe46dd43031e
anik112 <paulanik112@gmail.com>
2021-01-18 16:23:47 +0600

    Add new work.

--- report-14-01-2021.sql
+++ report-14-01-2021.sql
@@ -1,6 +1,6 @@
     select a.sectionnm , a.Total_Emp, b.Emp_0, c.Emp_60, d.Emp_120, e.Emp_180, f.Emp_240,
 	       g.Emp_300, h.Emp_360, i.Emp_420, j.Emp_480, k.Emp_540, l.Emp_600, m.Emp_660, 
-		   n.Emp_720
+		   n.Emp_720, o.Privious_ot, p.Avg_otrate
 	from 
 	(select count(info.cardno)Total_Emp,info.sectionnm
 	from   tb_personal_info info, tb_data_master mas
@@ -142,14 +142,26 @@
 	and    (mas.OTMIN + mas.OTPART) = 720
 	and    mas.cardno   = info.cardno
 	group by info.sectionnm)n,
-	(SELECT (SUM(OTMIN+OTPART)/60) INTO v_privious_ot 
+	(SELECT (SUM(OTMIN+OTPART)/60) Privious_ot ,info.sectionnm
 	from   tb_personal_info info, tb_data_master mas
 	where  info.company = :p_company
 	and    mas.company  = :p_com
 	and    mas.finyear  = :p_year
 	and    mas.finmonth = :p_month
+	and	   mas.PDATE BETWEEN to_date('1-'||:p_month||'-'||:p_year,'dd/mm/rrrr') AND (to_date(:p_date,'dd/mm/rrrr')-1)
+	and	   mas.OTMIN>0
 	and    mas.cardno   = info.cardno
-	AND	   mas.PDATE BETWEEN to_date('1-'||:p_month||'-'||:p_year,'dd/mm/rrrr') AND (to_date(:p_date,'dd/mm/rrrr')-1))
+	group by info.sectionnm)o,
+	(SELECT ROUND(SUM(OT_RATE(info.GROSSSALARY, info.WORKERTYPE))/COUNT(info.CARDNO),2) Avg_otrate, info.sectionnm
+	FROM   tb_personal_info info, tb_data_master mas
+	where  info.company = :p_company
+	and    mas.company  = :p_com
+	and    mas.finyear  = :p_year
+	and    mas.finmonth = :p_month
+	and    mas.pdate    = :p_date
+	and	   mas.OTMIN>0
+	and    mas.cardno   = info.cardno
+	group by info.sectionnm)p
 where a.sectionnm = b.sectionnm(+)
 and   a.sectionnm = c.sectionnm(+)
 and   a.sectionnm = d.sectionnm(+)
@@ -162,5 +174,23 @@ and	  a.sectionnm =	j.sectionnm(+)
 and	  a.sectionnm =	k.sectionnm(+)
 and	  a.sectionnm =	l.sectionnm(+)
 and	  a.sectionnm =	m.sectionnm(+)
-and	  a.sectionnm =	n.sectionnm(+)			
+and	  a.sectionnm =	n.sectionnm(+)
+and	  a.sectionnm =	o.sectionnm(+)
+and	  a.sectionnm =	p.sectionnm(+)		
 order by a.sectionnm asc
+
+
+
+-----------------
+
+
+SELECT (SUM(mas.OTMIN+mas.OTPART)/60) Privious_ot ,info.sectionnm
+	from   tb_personal_info info, tb_data_master mas
+	where  info.company = :p_company
+	and    mas.company  = :p_com
+	and    mas.finyear  = :p_year
+	and    mas.finmonth = :p_month
+	and	   mas.PDATE BETWEEN to_date('1-'||:p_month||'-'||:p_year,'dd/mm/rrrr') AND (to_date(:p_date,'dd/mm/rrrr')-1)
+	and	   mas.OTMIN>0
+	and    mas.cardno   = info.cardno
+	group by info.sectionnm
