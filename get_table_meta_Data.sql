91fa020efda09c5761f1a0c9e606eedf18dee770
anik112 <paulanik112@gmail.com>
2021-02-14 15:48:22 +0600

    Add new work.

---
+++ get_table_meta_Data.sql
@@ -0,0 +1,7 @@
+SELECT * FROM all_tables where owner='PAYROLL' and initial_extent > 16999;
+
+select * from user_tab_columns 
+
+select table_name, column_name, column_id  from user_tab_columns 
+where table_name in (SELECT table_name FROM all_tables where owner='PAYROLL')
+order by table_name, column_id asc
