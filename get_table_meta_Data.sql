511f8bef2fd2ce11f4d849fe36533fab7596b1f8
anik112 <paulanik112@gmail.com>
2021-02-17 19:35:21 +0600

    Add new work.

--- get_table_meta_Data.sql
+++ get_table_meta_Data.sql
@@ -3,5 +3,5 @@ SELECT * FROM all_tables where owner='PAYROLL' and initial_extent > 16999;
 select * from user_tab_columns 
 
 select table_name, column_name, column_id  from user_tab_columns 
-where table_name in (SELECT table_name FROM all_tables where owner='PAYROLL')
+where table_name in (SELECT table_name FROM all_tables where owner='payroll')
 order by table_name, column_id asc
