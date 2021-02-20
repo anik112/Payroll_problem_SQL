f2e5fbdb502ecd31554192289fccf6db5fc07046
anik112 <paulanik112@gmail.com>
2021-02-20 19:22:17 +0600

    Add new work.

---
+++ Employee_Details.sql
@@ -0,0 +1,129 @@
+SELECT SECTIONNM,DEPARTMENTNM,lineno,cardno,secreteno,empname,designation,joining_date,(grosssalary + GROSS_BK)grosssalary,WORKERTYPE
+FROM tb_personal_info
+WHERE company = :p_company
+AND   DEPARTMENTNM LIKE DECODE(NVL(:p_deptname,'all'),'all','%',:p_deptname)
+AND   SECTIONNM    LIKE DECODE(NVL(:p_section,'all'),'all','%',:p_section)
+AND   DESIGNATION  LIKE DECODE(NVL(:p_desig,'all'),'all','%',:p_desig)
+AND   WORKERTYPE   LIKE DECODE(NVL(:p_worker,'all'),'all','%',:p_worker)
+AND   FLOORNO      LIKE DECODE(NVL(:p_floorno,'all'),'all','%',:p_floorno)
+AND   MACHINENO    LIKE DECODE(NVL(:p_machineno,'all'),'all','%',:p_machineno)	
+AND   lineno       LIKE DECODE(NVL(:p_lineno,'all'),'all','%',:p_lineno)
+AND   SHIFT        LIKE DECODE(NVL(:p_shift,'all'),'all','%',:p_shift) 
+AND   GENDER       LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender) 
+AND   CASH_TYPE    LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME    LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE     LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   cardno       LIKE DECODE(NVL(:p_cardno,'all'),'all','%',:p_cardno) 
+AND   active       LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+ORDER BY DEPARTMENTNM,SECTIONNM,lineno,cardno ASC
+
+
+---------------- basic func  
+FUNCTION CF_Basic_SalFormula RETURN NUMBER IS
+
+v_basic   NUMBER:= 0;
+
+BEGIN
+  
+  BEGIN
+  v_basic := Basic_Salary(:p_company,:GROSSSALARY,:WORKERTYPE);
+  EXCEPTION
+		WHEN OTHERS THEN v_basic := 0;
+  END;
+  
+  RETURN v_basic;
+
+EXCEPTION
+	WHEN OTHERS THEN NULL;
+END;
+
+--------------------------------  SLect SQL ----
+
+SELECT SECTIONNM,DEPARTMENTNM,lineno,cardno,secreteno,empname,designation,joining_date,(grosssalary + GROSS_BK)grosssalary,WORKERTYPE
+FROM tb_personal_info 
+WHERE company = :p_company
+AND   DEPARTMENTNM IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT SECTIONNM,DEPARTMENTNM,lineno,cardno,secreteno,empname,designation,joining_date,(grosssalary + GROSS_BK)grosssalary,WORKERTYPE
+FROM tb_personal_info 
+WHERE company = :p_company
+AND   SECTIONNM IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT SECTIONNM,DEPARTMENTNM,lineno,cardno,secreteno,empname,designation,joining_date,(grosssalary + GROSS_BK)grosssalary,WORKERTYPE
+FROM tb_personal_info 
+WHERE company = :p_company
+AND   designation IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT SECTIONNM,DEPARTMENTNM,lineno,cardno,secreteno,empname,designation,joining_date,(grosssalary + GROSS_BK)grosssalary,WORKERTYPE
+FROM tb_personal_info 
+WHERE company = :p_company
+AND   WORKERTYPE IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT SECTIONNM,DEPARTMENTNM,lineno,cardno,secreteno,empname,designation,joining_date,(grosssalary + GROSS_BK)grosssalary,WORKERTYPE
+FROM tb_personal_info  
+WHERE company = :p_company
+AND   MACHINENO IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT SECTIONNM,DEPARTMENTNM,lineno,cardno,secreteno,empname,designation,joining_date,(grosssalary + GROSS_BK)grosssalary,WORKERTYPE
+FROM tb_personal_info 
+WHERE company = :p_company
+AND   FLOORNO   IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT SECTIONNM,DEPARTMENTNM,lineno,cardno,secreteno,empname,designation,joining_date,(grosssalary + GROSS_BK)grosssalary,WORKERTYPE
+FROM tb_personal_info
+WHERE company  = :p_company
+AND   lineno    IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT SECTIONNM,DEPARTMENTNM,lineno,cardno,secreteno,empname,designation,joining_date,(grosssalary + GROSS_BK)grosssalary,WORKERTYPE
+FROM tb_personal_info 
+WHERE company = :p_company
+AND   SHIFT     IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT info.SECTIONNM,info.DEPARTMENTNM,info.lineno,info.cardno,info.secreteno,info.empname,
+       info.designation,info.joining_date,(info.grosssalary + info.GROSS_BK)grosssalary,info.WORKERTYPE
+FROM tb_personal_info info , TB_IDCARD_MULTIPLE mul
+WHERE info.company  = :p_company
+AND   info.company  = mul.company
+AND   mul.USER_NAME = :p_user
+AND   info.cardno   = mul.cardno
+ORDER BY DEPARTMENTNM,SECTIONNM,lineno,cardno ASC
---
+++ Employee_Details_F2.sql
@@ -0,0 +1,109 @@
+SELECT DEPARTMENTNM, SECTIONNM, lineno, cardno, empname, FATHER_NAME, designation, BIRTH_DATE, joining_date
+FROM tb_personal_info
+WHERE company =:p_company
+AND   DEPARTMENTNM LIKE DECODE(NVL(:p_deptname,'all'),'all','%',:p_deptname)
+AND   SECTIONNM    LIKE DECODE(NVL(:p_section,'all'),'all','%',:p_section)
+AND   DESIGNATION  LIKE DECODE(NVL(:p_desig,'all'),'all','%',:p_desig)
+AND   WORKERTYPE   LIKE DECODE(NVL(:p_worker,'all'),'all','%',:p_worker)
+AND   FLOORNO      LIKE DECODE(NVL(:p_floorno,'all'),'all','%',:p_floorno)
+AND   MACHINENO    LIKE DECODE(NVL(:p_machineno,'all'),'all','%',:p_machineno)
+AND   lineno       LIKE DECODE(NVL(:p_lineno,'all'),'all','%',:p_lineno)
+AND   SHIFT        LIKE DECODE(NVL(:p_shift,'all'),'all','%',:p_shift) 
+AND   GENDER       LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender) 
+AND   CASH_TYPE    LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype) 
+AND   BANK_NAME    LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname)
+AND   MFT_TYPE     LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type) 
+AND   cardno       LIKE DECODE(NVL(:p_cardno,'all'),'all','%',:p_cardno) 
+AND   active       LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+ORDER BY DEPARTMENTNM, SECTIONNM, lineno, cardno ASC
+
+
+------------------------------------------- Select 
+
+SELECT DEPARTMENTNM, SECTIONNM, lineno, cardno, empname, FATHER_NAME, designation, BIRTH_DATE, joining_date
+FROM tb_personal_info 
+WHERE company = :p_company
+AND   DEPARTMENTNM IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT DEPARTMENTNM, SECTIONNM, lineno, cardno, empname, FATHER_NAME, designation, BIRTH_DATE, joining_date
+FROM tb_personal_info 
+WHERE company = :p_company
+AND   SECTIONNM IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT DEPARTMENTNM, SECTIONNM, lineno, cardno, empname, FATHER_NAME, designation, BIRTH_DATE, joining_date
+FROM tb_personal_info 
+WHERE company = :p_company
+AND   designation IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT DEPARTMENTNM, SECTIONNM, lineno, cardno, empname, FATHER_NAME, designation, BIRTH_DATE, joining_date
+FROM tb_personal_info 
+WHERE company = :p_company
+AND   WORKERTYPE IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT DEPARTMENTNM, SECTIONNM, lineno, cardno, empname, FATHER_NAME, designation, BIRTH_DATE, joining_date
+FROM tb_personal_info  
+WHERE company = :p_company
+AND   MACHINENO IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT DEPARTMENTNM, SECTIONNM, lineno, cardno, empname, FATHER_NAME, designation, BIRTH_DATE, joining_date
+FROM tb_personal_info 
+WHERE company = :p_company
+AND   FLOORNO   IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT DEPARTMENTNM, SECTIONNM, lineno, cardno, empname, FATHER_NAME, designation, BIRTH_DATE, joining_date
+FROM tb_personal_info
+WHERE company  = :p_company
+AND   lineno    IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT DEPARTMENTNM, SECTIONNM, lineno, cardno, empname, FATHER_NAME, designation, BIRTH_DATE, joining_date
+FROM tb_personal_info 
+WHERE company = :p_company
+AND   SHIFT     IN ( SELECT ITEM_NAME FROM TB_SETUP_ITEM WHERE company =:p_company AND ITEM_NAME IS NOT NULL AND USER_NAME = :p_user)
+AND   GENDER    LIKE DECODE(NVL(:p_gender,'all'),'all','%',:p_gender)
+AND   CASH_TYPE LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype)
+AND   BANK_NAME LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
+AND   MFT_TYPE  LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type)
+AND   active    LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
+UNION
+SELECT info.DEPARTMENTNM, info.SECTIONNM, info.lineno, info.cardno, info.empname, info.FATHER_NAME, info.designation, info.BIRTH_DATE, info.joining_date
+FROM tb_personal_info info, TB_IDCARD_MULTIPLE mul
+WHERE info.company  = :p_company
+AND   info.company  = mul.company
+AND   mul.USER_NAME = :p_user
+AND   info.cardno   = mul.cardno
+ORDER BY DEPARTMENTNM, SECTIONNM, lineno, cardno ASC
