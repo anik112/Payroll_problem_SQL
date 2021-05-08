dde662d4d8d822e15bcc42a20d3c3dc60439e518
anik112 <paulanik112@gmail.com>
2021-05-08 16:11:53 +0600

    Add new work.

--- Monthly_OTAmount.sql
+++ Monthly_OTAmount.sql
@@ -1,6 +1,7 @@
-SELECT sal.cardno, sal.empname, sal.enmname_bangla, sal.designation, sal.designation_ban,  (sal.basicsal_ch + sal.basicsal_bk)basicsal, (sal.grosssal_ch + grosssal_bk)grosssal, sal.otamount, 
-               sal.departmentnm,  sal.dept_bangla,  sal.lineno, sal.sectionnm, sal.sec_bangla, sal.otrate, mon.othr, per.joining_date, per.shift
-FROM   tb_month_salary sal,tb_month_totalday  mon, tb_personal_info per 
+SELECT sal.cardno, sal.empname, sal.enmname_bangla, sal.designatio, sal.designation_ban,  (sal.basicsal_ch + sal.basicsal_bk)basicsal, 
+       (sal.grosssal_ch + grosssal_bk)grosssal, sal.otamount, sal.departmentnm,  sal.dept_bangla,  sal.lineno, sal.sectionnm, sal.sec_bangla,
+	   sal.otrate, mon.othr, per.joining_date, per.shift
+FROM   TB_MONTH_SALARY sal,TB_MONTH_TOTALDAY  mon, TB_PERSONAL_INFO per 
 WHERE  mon.company  = :p_company 
 AND    sal.company        = per.company
 AND    mon.company      = per.company   
@@ -16,13 +17,13 @@ AND    per.sectionnm       LIKE DECODE(NVL(:p_section,'all'),'all','%',:p_sectio
 AND    per.designation     LIKE DECODE(NVL(:p_designation,'all'),'all','%',:p_designation) 
 AND    per.workertype      LIKE DECODE(NVL(:p_woker,'all'),'all','%',:p_woker)
 AND    per.floorno             LIKE DECODE(NVL(:p_floorno,'all'),'all','%',:p_floorno) 
-AND    per.machineno       LIKE DECODE(NVL(:p_machineno,'all'),'all','%',:p_machineno)
+AND    per.machineno      LIKE DECODE(NVL(:p_machineno,'all'),'all','%',:p_machineno)
 AND    per.lineno              LIKE DECODE(NVL(:p_line,'all'),'all','%',:p_line) 
 AND    per.shift                 LIKE DECODE(NVL(:p_shift,'all'),'all','%',:p_shift)
 AND    per.cash_type       LIKE DECODE(NVL(:p_paytype,'all'),'all','%',:p_paytype) 
 AND    per.bank_name     LIKE DECODE(NVL(:p_bkname,'all'),'all','%',:p_bkname) 
 AND    per.mft_type          LIKE DECODE(NVL(:p_mft_type,'all'),'all','%',:p_mft_type) 
-AND    sal.sal_status         LIKE DECODE(NVL(:p_sal_status,'all'),'all','%',:p_sal_status)
+AND    sal.sal_status        LIKE DECODE(NVL(:p_sal_status,'all'),'all','%',:p_sal_status)
 AND    sal.pay_status       LIKE DECODE(NVL(:p_pay_status,'all'),'all','%',:p_pay_status)
 AND    sal.cardno             LIKE DECODE(NVL(:p_card,'all'),'all','%',:p_card)
 AND    per.active              LIKE DECODE(NVL(:p_active,'all'),'all','%',:p_active)
